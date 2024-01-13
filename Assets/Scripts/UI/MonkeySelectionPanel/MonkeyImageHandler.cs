d5d640a1859cc11112069b2e11fd90d43e38bec1
Aayush Gupta <aayush.gupta555@gmail.com>
2024-01-14 01:25:47 +0530

    Implementing Draggable

--- Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
+++ Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
@@ -6,11 +6,24 @@ using UnityEngine.UI;
 
 namespace ServiceLocator.UI
 {
-    public class MonkeyImageHandler : MonoBehaviour
+    public class MonkeyImageHandler : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerDownHandler
     {
         private Image monkeyImage;
         private MonkeyCellController owner;
         private Sprite spriteToSet;
+        private RectTransform rectTransform;
+        private Canvas canvas;
+        private Vector3 OgPosition, OgAnchoredPosition;
+
+        private void Awake()
+        {
+            monkeyImage = GetComponent<Image>();
+            monkeyImage.sprite = spriteToSet;
+            rectTransform = GetComponent<RectTransform>();
+            canvas = FindAnyObjectByType<Canvas>();
+            OgAnchoredPosition = rectTransform.anchoredPosition;
+            OgPosition = rectTransform.position;
+        }
 
         public void ConfigureImageHandler(Sprite spriteToSet, MonkeyCellController owner)
         {
@@ -18,10 +31,32 @@ namespace ServiceLocator.UI
             this.owner = owner;
         }
 
-        private void Awake()
+        public void OnDrag(PointerEventData eventData)
         {
-            monkeyImage = GetComponent<Image>();
-            monkeyImage.sprite = spriteToSet;
+            rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+            owner.MonkeyDraggedAt(rectTransform.position);
+            
         }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            owner.MonkeyDroppedAt(rectTransform.position);
+            ResetMonkey();
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            monkeyImage.color = new Color(1, 1, 1, 0.5f);
+        }
+
+        private void ResetMonkey()
+        {
+            monkeyImage.color = new Color(1, 1, 1, 1f);
+            rectTransform.position = OgPosition;
+            rectTransform.anchoredPosition = OgAnchoredPosition;
+            GetComponent<LayoutElement>().enabled = false;
+            GetComponent<LayoutElement>().enabled = true;
+        }
+        
     }
 }
